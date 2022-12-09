71164e6e5ae1eb68694b4d807f380f1db9cb5d20
melvin <melvinn721@gmail.com>
2022-12-09 23:04:08 +1300

    update form

--- src/components/WeightTracker.tsx
+++ src/components/WeightTracker.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import moment from "moment";
 import "./weightTracker.css";
-import { TextField, InputAdornment } from "@mui/material";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 type TweightInput = {
   Weight: string;
@@ -11,40 +13,51 @@ type TweightInput = {
 };
 
 export const WeightTracker = (): JSX.Element => {
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      Weight: "",
-      Date: moment().format("DD-MM-YYYY"),
-    },
-  });
-  console.log(errors);
-  const now = Date.now();
-  const [startDate, setStartDate] = useState<Date | null>(new Date(now));
+  const now = moment().format("YYYY-MM-DD");
+  console.log(now);
+  const [date, setDate] = useState<string>(now);
+  const [weight, setWeight] = useState<number | undefined>();
+
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
   return (
     <div className="weight-tracker__input-form-container">
-      <form
-        className="weight-tracker__input-form"
-        onSubmit={handleSubmit((data) => {
-          console.log(data);
-        })}
-      >
-        <input {...register("Weight", {})} />
+      <form className="weight-tracker__input-form">
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Weight"
+            aria-label="Weight input"
+            aria-describedby="basic-addon2"
+            type="number"
+            value={weight}
+            autoComplete="off"
+            required
+            onChange={(e) => {
+              !isNaN(Number(e.target.value)) ? (
+                setWeight(Number(e.target.value))
+              ) : (
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid city.
+                </Form.Control.Feedback>
+              );
+            }}
+          />
+          <InputGroup.Text id="basic-addon2">kg</InputGroup.Text>
+        </InputGroup>
+
         {/* react hook form uses uncontrolled inputs (not dependant on state), to */}
         {/* use controlled inputs e.g like DatePicker, need to use Controller */}
-        <Controller
-          name="Date"
-          control={control}
-          render={() => (
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-            />
-          )}
+
+        <Form.Control
+          className="weight-tracker__date-picker"
+          type="date"
+          value={date}
+          onChange={(e) => {
+            e.preventDefault();
+            setDate(e.target.value);
+          }}
+          required
         />
         {/* <input {...register("Date", { required: "Date is required" })} /> */}
         <input type="submit" />
